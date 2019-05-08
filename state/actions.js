38702d74a067eb69101bda495f9d66c11e60f352
Marcel Cutts <marcel.cutts@gmail.com>
2019-05-08 12:24:05 +0100

    Add async reducers

--- state/actions.js
+++ state/actions.js
@@ -16,3 +16,30 @@ export const setUser = payload => ({
 export const clearUser = () => ({
   type: "CLEAR_USER"
 });
+
+export const sendPatientsRequest = () => ({
+  type: "SEND_PATIENTS_REQUEST"
+});
+
+export const sendPatientsSuccess = () => ({
+  type: "SEND_PATIENTS_SUCCESS"
+});
+
+export const sendPatientsFailure = error => ({
+  type: "SEND_PATIENTS_FAILURE",
+  error
+});
+
+export const uploadPatients = () => {
+  return (dispatch, getState) => {
+    dispatch(sendPatientsRequest());
+    fetch("https://postman-echo.com/post", {
+      method: "POST",
+      body: JSON.stringify(getState().patients.queue)
+    }).then(response => {
+      if (response.status === 200) {
+        dispatch(sendPatientsSuccess());
+      }
+    });
+  };
+};
--- state/configureStore.js
+++ state/configureStore.js
@@ -1,7 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web and AsyncStorage for react-native
-
 import { rootReducer } from "./reducers";
 
 const persistConfig = {
@@ -12,7 +12,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default () => {
-  let store = createStore(persistedReducer);
+  let store = createStore(persistedReducer, applyMiddleware(thunk));
   let persistor = persistStore(store);
   return { store, persistor };
 };
