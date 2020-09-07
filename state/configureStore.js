3c35eaf97265e06be48875bf55f60f6d87b20610
Marcel Cutts <marcel.cutts@gmail.com>
2020-09-07 12:10:01 +0100

    Upgrade persistence to modern versions

--- state/configureStore.js
+++ state/configureStore.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import thunk from "redux-thunk";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web and AsyncStorage for react-native
 import { rootReducer } from "./reducers";
+
+import AsyncStorage from "@react-native-community/async-storage";
 
 const persistConfig = {
   key: "root",
-  storage
+  storage: AsyncStorage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
